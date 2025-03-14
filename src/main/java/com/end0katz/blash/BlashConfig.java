dd81062e340945e959d9cf612c7b28a6fb10b057
End0katz <endokatz@gmail.com>
2025-03-14 07:10:29 +0000

    Added some options to BlashConfig and BlashContext

--- src/main/java/com/end0katz/blash/BlashConfig.java
+++ src/main/java/com/end0katz/blash/BlashConfig.java
@@ -1,11 +1,16 @@
 package com.end0katz.blash;
 
-public record BlashConfig() {
+import java.nio.file.*;
+
+public record BlashConfig(
+        Path initialCWD) {
 
     public static class Builder {
 
+        Path cwd = Paths.get(System.getProperty("user.home"));
+
         public BlashConfig build() {
-            return new BlashConfig();
+            return new BlashConfig(cwd);
         }
     }
 }
--- src/main/java/com/end0katz/blash/BlashContext.java
+++ src/main/java/com/end0katz/blash/BlashContext.java
@@ -1,20 +1,43 @@
 package com.end0katz.blash;
 
+import java.nio.file.*;
+
 public record BlashContext(
-        boolean root) {
+        boolean root,
+        String username,
+        Path userHome) {
 
     public static class Builder {
 
         boolean root = false;
+        String username = System.getProperty("user.name");
+        Path userHome = Paths.get(System.getProperty("user.home"));
 
         public Builder sudo() {
             root = true;
             return this;
         }
 
+        public Builder sudo(boolean value) {
+            root = value;
+            return this;
+        }
+
+        public Builder name(String s) {
+            username = s;
+            return this;
+        }
+
+        public Builder homeDir(Path p) {
+            userHome = p;
+            return this;
+        }
+
         public BlashContext build() {
             return new BlashContext(
-                    root
+                    root,
+                    username,
+                    userHome
             );
         }
     }
